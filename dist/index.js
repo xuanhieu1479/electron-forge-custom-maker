f558ba80e784c061a0920c47f44df14d19a1bda7
Xuan Hieu <xuanhieu1479@gmail.com>
2021-02-26 22:00:21 +0700

    fix bug cannot unlink

--- dist/index.js
+++ dist/index.js
@@ -11,14 +11,14 @@ const archiver_1 = __importDefault(require("archiver"));
 class MakerSquirrel extends maker_base_1.default {
     constructor() {
         super(...arguments);
-        this.name = 'squirrel';
-        this.defaultPlatforms = ['win32'];
+        this.name = "squirrel";
+        this.defaultPlatforms = ["win32"];
     }
     isSupportedOnCurrentPlatform() {
-        return (this.isInstalled('electron-winstaller') &&
+        return (this.isInstalled("electron-winstaller") &&
             !process.env.DISABLE_SQUIRREL_TEST);
     }
-    async make({ dir, makeDir, targetArch, packageJSON, appName }) {
+    async make({ dir, makeDir, targetArch, packageJSON, appName, }) {
         const outPath = path_1.default.resolve(makeDir, `squirrel.windows/${targetArch}`);
         await this.ensureDirectory(outPath);
         const winstallerConfig = {
@@ -29,14 +29,14 @@ class MakerSquirrel extends maker_base_1.default {
             setupExe: `${appName}-${packageJSON.version} Setup.exe`,
             ...this.config,
             appDirectory: dir,
-            outputDirectory: outPath
+            outputDirectory: outPath,
         };
         await electron_winstaller_1.createWindowsInstaller(winstallerConfig);
         const nupkgVersion = electron_winstaller_1.convertVersion(packageJSON.version);
         const artifacts = [
-            path_1.default.resolve(outPath, 'RELEASES'),
+            path_1.default.resolve(outPath, "RELEASES"),
             path_1.default.resolve(outPath, winstallerConfig.setupExe || `${appName}Setup.exe`),
-            path_1.default.resolve(outPath, `${winstallerConfig.name}-${nupkgVersion}-full.nupkg`)
+            path_1.default.resolve(outPath, `${winstallerConfig.name}-${nupkgVersion}-full.nupkg`),
         ];
         // Delta Path is for auto updater
         const deltaPath = path_1.default.resolve(outPath, `${winstallerConfig.name}-${nupkgVersion}-delta.nupkg`);
@@ -49,19 +49,19 @@ class MakerSquirrel extends maker_base_1.default {
         }
         const zipName = `${appName}-${packageJSON.version} Setup.zip`;
         const zipPath = `${outPath}\\${zipName}`;
-        const archive = archiver_1.default('zip', { zlib: { level: 0 } });
+        const archive = archiver_1.default("zip", { zlib: { level: 0 } });
         archive.pipe(fs_1.default.createWriteStream(zipPath));
-        artifacts.forEach(artifact => {
-            const tmp = artifact.split('\\');
+        artifacts.forEach((artifact) => {
+            const tmp = artifact.split("\\");
             const name = tmp[tmp.length - 1];
             archive.file(artifact, { name });
         });
         await archive.finalize();
         const files = fs_1.default.readdirSync(outPath, { withFileTypes: true });
-        files.forEach(file => {
+        files.forEach((file) => {
             const { name } = file;
-            if (!name.includes(zipName))
-                fs_1.default.unlinkSync(name);
+            if (name !== zipName)
+                fs_1.default.unlinkSync(`${outPath}\\${name}`);
         });
         return [zipPath];
     }
--- index.ts
+++ index.ts
@@ -1,27 +1,27 @@
-import MakerBase, { MakerOptions } from '@electron-forge/maker-base';
-import { ForgePlatform } from '@electron-forge/shared-types';
+import MakerBase, { MakerOptions } from "@electron-forge/maker-base";
+import { ForgePlatform } from "@electron-forge/shared-types";
 import {
   convertVersion,
   createWindowsInstaller,
-  Options as ElectronWinstallerOptions
-} from 'electron-winstaller';
-import fs from 'fs';
-import path from 'path';
-import archiver from 'archiver';
+  Options as ElectronWinstallerOptions,
+} from "electron-winstaller";
+import fs from "fs";
+import path from "path";
+import archiver from "archiver";
 
 type MakerSquirrelConfig = Omit<
   ElectronWinstallerOptions,
-  'appDirectory' | 'outputDirectory'
+  "appDirectory" | "outputDirectory"
 >;
 
 export default class MakerSquirrel extends MakerBase<MakerSquirrelConfig> {
-  name = 'squirrel';
+  name = "squirrel";
 
-  defaultPlatforms: ForgePlatform[] = ['win32'];
+  defaultPlatforms: ForgePlatform[] = ["win32"];
 
   isSupportedOnCurrentPlatform(): boolean {
     return (
-      this.isInstalled('electron-winstaller') &&
+      this.isInstalled("electron-winstaller") &&
       !process.env.DISABLE_SQUIRREL_TEST
     );
   }
@@ -31,7 +31,7 @@ export default class MakerSquirrel extends MakerBase<MakerSquirrelConfig> {
     makeDir,
     targetArch,
     packageJSON,
-    appName
+    appName,
   }: MakerOptions): Promise<string[]> {
     const outPath = path.resolve(makeDir, `squirrel.windows/${targetArch}`);
     await this.ensureDirectory(outPath);
@@ -44,7 +44,7 @@ export default class MakerSquirrel extends MakerBase<MakerSquirrelConfig> {
       setupExe: `${appName}-${packageJSON.version} Setup.exe`,
       ...this.config,
       appDirectory: dir,
-      outputDirectory: outPath
+      outputDirectory: outPath,
     };
 
     await createWindowsInstaller(winstallerConfig);
@@ -52,12 +52,12 @@ export default class MakerSquirrel extends MakerBase<MakerSquirrelConfig> {
     const nupkgVersion = convertVersion(packageJSON.version);
 
     const artifacts = [
-      path.resolve(outPath, 'RELEASES'),
+      path.resolve(outPath, "RELEASES"),
       path.resolve(outPath, winstallerConfig.setupExe || `${appName}Setup.exe`),
       path.resolve(
         outPath,
         `${winstallerConfig.name}-${nupkgVersion}-full.nupkg`
-      )
+      ),
     ];
     // Delta Path is for auto updater
     const deltaPath = path.resolve(
@@ -77,19 +77,19 @@ export default class MakerSquirrel extends MakerBase<MakerSquirrelConfig> {
 
     const zipName = `${appName}-${packageJSON.version} Setup.zip`;
     const zipPath = `${outPath}\\${zipName}`;
-    const archive = archiver('zip', { zlib: { level: 0 } });
+    const archive = archiver("zip", { zlib: { level: 0 } });
     archive.pipe(fs.createWriteStream(zipPath));
-    artifacts.forEach(artifact => {
-      const tmp = artifact.split('\\');
+    artifacts.forEach((artifact) => {
+      const tmp = artifact.split("\\");
       const name = tmp[tmp.length - 1];
       archive.file(artifact, { name });
     });
     await archive.finalize();
 
     const files = fs.readdirSync(outPath, { withFileTypes: true });
-    files.forEach(file => {
+    files.forEach((file) => {
       const { name } = file;
-      if (!name.includes(zipName)) fs.unlinkSync(name);
+      if (name !== zipName) fs.unlinkSync(`${outPath}\\${name}`);
     });
 
     return [zipPath];
